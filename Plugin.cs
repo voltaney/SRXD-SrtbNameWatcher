be2870169df4683e5419fb9bdbb0ba5c21278ffb
voltaney <voltaney61@gmail.com>
2024-06-01 17:19:41 +0900

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.IO;
+
+namespace SrtbNameWatcher
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Patch), PluginInfo.PLUGIN_GUID);
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded");
+        }
+    }
+    public class Patch
+    {
+        [HarmonyPatch(typeof(XDSelectionListItemDisplay_Track), "PlayOnCurrentDifficulty")]
+        [HarmonyPrefix]
+        private static void Play_Prefix(XDSelectionListItemDisplay_Track __instance)
+        {
+            ITrackItem trackItem = __instance.Item.item as ITrackItem;
+            TrackInfoAssetReference trackInfoRef = trackItem.Metadata.TrackInfoRef;
+            IMultiAssetSaveFile multiAssetSaveFile = (trackInfoRef != null) ? trackInfoRef.customFile : null;
+
+            if (multiAssetSaveFile != null)
+            {
+
+                using (var writer = new StreamWriter("CurrentSrtbName.info", false))
+                {
+                    writer.WriteLine($"{multiAssetSaveFile.FileNameNoExtension}");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Srtb Name Watcher
+
+カスタムチャートがプレイされた際、そのファイルリファレンス名を Spin Rhythm XD のディレクトリの **`CurrentSrtbName.info`** に保存する MOD。
+
+If you monitor the file with watchdog, etc., you can get information on the song currently being played. Might be useful for something...
+
+### Example
+
+```
+spinshare_5fe914f872e99
+```
+
+## Requirements
+
+This mod works with [BepInEx5](https://github.com/BepInEx/BepInEx/releases/tag/v5.4.21).
+Please refer to [this repository](https://github.com/SRXDModdingGroup/SpinSearch) for a detailed description of how to install BepInEx on Spin Rhythm XD.
